03a4193ed27e90c48ab3253b4d7a5ff7bab5f790
rhdqors <108318494+rhdqors@users.noreply.github.com>
2023-08-26 16:57:56 +0900

    Time: 2 ms (57.26%), Space: 45.9 MB (17.54%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -4,17 +4,17 @@ class Solution {
         int right = numbers.length-1;
         int[] result = new int[2];
         
-        if (numbers[left] + numbers[right] == target) {
-            result[0] = left+1;
-            result[1] = right+1;
-        }  
+        // if (numbers[left] + numbers[right] == target) {
+        //     result[0] = left+1;
+        //     result[1] = right+1;
+        // }  
         
         while (left < right) {
-            if (numbers[left] + numbers[right] > target) {
-                right--;
-            } else {
-                left++;
-            }
+            // if (numbers[left] + numbers[right] > target) {
+            //     right--;
+            // } else {
+            //     left++;
+            // }
             
             if (numbers[left] + numbers[right] == target) {
                 if (left == right) {
@@ -25,6 +25,10 @@ class Solution {
                 result[0] = left+1;
                 result[1] = right+1;
                 break;
+            } else if (numbers[left] + numbers[right] > target) {
+                right--;
+            } else {
+                left++;
             }
         }
         
