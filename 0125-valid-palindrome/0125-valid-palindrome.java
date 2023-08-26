2f549189ea04c64c492c7c773832ee69d9862e31
rhdqors <108318494+rhdqors@users.noreply.github.com>
2023-08-26 15:35:22 +0900

    Time: 22 ms (34.53%), Space: 43.7 MB (53.67%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,16 +1,11 @@
 class Solution {
     public boolean isPalindrome(String s) {
         s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        System.out.println("s = " + s);
-        
-        // String reverse = "";
         StringBuilder reverse = new StringBuilder();
 
         for (int i = s.length() - 1; i >= 0; i--) {
-            // reverse += s.charAt(i);
             reverse.append(s.charAt(i));
         }
-        System.out.println("reverse = " + reverse);
         if (!reverse.toString().equals(s)) {
             return false;
         }
