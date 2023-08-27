94349c0b0eddcb944852b95296de56639284da77
rhdqors <108318494+rhdqors@users.noreply.github.com>
2023-08-28 03:52:43 +0900

    Time: 6 ms (78.12%), Space: 43.4 MB (82.25%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -7,18 +7,21 @@ class Solution {
         
         for (int right=0; right<s.length(); right++) {
             while (set.contains(s.charAt(right))) {
-                // System.out.println("1 set = " + set);
+                // System.out.println("3set = " + set);
                 set.remove(s.charAt(left));
-                // System.out.println("2 set = " + set);
+                // System.out.println("4set = " + set);
+                // System.out.println("max = " + max);
                 left++;
             }
-            // System.out.println("3 set = " + set);
+            
+            // System.out.println("1set = " + set);
             set.add(s.charAt(right));
-            // System.out.println("4 set = " + set);
+            // System.out.println("2set = " + set);
             max = Math.max(max, set.size());
-            // System.out.println("max = " + max);
+            
+            
         }
-        
+        // return set.size();
         return max;
     }
 }
