dbf3e261365a50322155a0a00d892fb1e8f27b94
rhdqors <108318494+rhdqors@users.noreply.github.com>
2023-08-28 03:52:56 +0900

    Time: 7 ms (57.72%), Space: 43.3 MB (87.18%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,27 +1,24 @@
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        
-        int max = 0;
-        int left = 0;
-        HashSet<Character> set = new HashSet<>();
-        
-        for (int right=0; right<s.length(); right++) {
-            while (set.contains(s.charAt(right))) {
-                // System.out.println("3set = " + set);
-                set.remove(s.charAt(left));
-                // System.out.println("4set = " + set);
-                // System.out.println("max = " + max);
-                left++;
-            }
-            
-            // System.out.println("1set = " + set);
-            set.add(s.charAt(right));
-            // System.out.println("2set = " + set);
-            max = Math.max(max, set.size());
-            
-            
-        }
-        // return set.size();
-        return max;
-    }
+ public int lengthOfLongestSubstring(String s) {
+
+     int max = 0;
+     int left = 0;
+     HashSet<Character> set = new HashSet<>();
+
+     for (int right=0; right<s.length(); right++) {
+         while (set.contains(s.charAt(right))) {
+             // System.out.println("1 set = " + set);
+             set.remove(s.charAt(left));
+             // System.out.println("2 set = " + set);
+             left++;
+         }
+         // System.out.println("3 set = " + set);
+         set.add(s.charAt(right));
+         // System.out.println("4 set = " + set);
+         max = Math.max(max, set.size());
+         // System.out.println("max = " + max);
+     }
+
+     return max;
+ }
 }
