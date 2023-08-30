89c33050baddd1286e2ebac27f2e42f5ad735453
rhdqors <108318494+rhdqors@users.noreply.github.com>
2023-08-30 14:16:59 +0900

    Time: 0 ms (100.00%), Space: 43 MB (90.35%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,34 +1,34 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-//         int left = 0;
-//         int right = nums.length-1;
+        int left = 0;
+        int right = nums.length-1;
         
-//         while (left <= right) {
-//             int mid = left + (right - left) / 2;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
             
-//             if (target == nums[mid]) {
-//                 return mid;
-//             } else if (target > mid) {
-//                 left = mid+1;
-//             } else {
-//                 right = mid-1;
-//             }
-//         }
-
-//         return left;
-        
-        
-        
-        int start = 0;
-        int end = nums.length-1;
-
-        while (start <= end) {
-            int mid = start + (end-start)/2;
-            if (nums[mid] == target) return mid;
-            else if (nums[mid] > target) end = mid-1;
-            else start = mid+1;
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target > nums[mid]) {
+                left = mid+1;
+            } else {
+                right = mid-1;
+            }
         }
 
-        return start;
+        return left;
+        
+        
+        
+//         int start = 0;
+//         int end = nums.length-1;
+
+//         while (start <= end) {
+//             int mid = start + (end-start)/2;
+//             if (nums[mid] == target) return mid;
+//             else if (nums[mid] > target) end = mid-1;
+//             else start = mid+1;
+//         }
+
+//         return start;
     }
 }
