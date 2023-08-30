491c599437b5d0c3f8ead29479de920326a4e81a
rhdqors <108318494+rhdqors@users.noreply.github.com>
2023-08-30 12:46:15 +0900

    Time: 237 ms (5.36%), Space: 44.6 MB (5.42%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,37 +10,82 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummyHead = new ListNode(0);
-        ListNode tail = dummyHead;
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
         int carry = 0;
-        
-        System.out.println("dummyHead data = " + dummyHead.val);
-        System.out.println("dummyHead 주소값 = " + dummyHead);
-        System.out.println("tail 주소값 = " + tail);
+        System.out.println("처음 dummy = " + dummy);
         
         while (l1 != null || l2 != null || carry != 0) {
             int num1 = l1 == null ? 0 : l1.val;
             int num2 = l2 == null ? 0 : l2.val;
             int sum = num1 + num2 + carry;
-            int val = sum % 10;
             carry = sum / 10;
+            int val = sum % 10;
+            System.out.println("val = " + val);
+            System.out.println("carry = " + carry);
+            System.out.println("dummy = " + dummy);
             System.out.println("num1 = " + num1);
             System.out.println("num2 = " + num2);
             System.out.println("sum = " + sum);
             
             ListNode newNode = new ListNode(val);
-            tail.next = newNode;
-            tail = tail.next;
-            
             System.out.println("newNode = " + newNode);
             System.out.println("newNode.val = " + newNode.val);
-            System.out.println("1 tail 주소값 = " + tail);
-            System.out.println("tail.next 주소값 = " + tail.next);
+            tail.next = newNode;
+            tail = newNode;
+
+            System.out.println("tail = " + tail);
+            System.out.println("tail = " + tail.next);
             
             l1 = (l1 != null) ? l1.next : null;
             l2 = (l2 != null) ? l2.next : null;
+            
         }
         
-        return dummyHead.next;
+        return dummy.next;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         ListNode dummyHead = new ListNode(0);
+//         ListNode tail = dummyHead;
+//         int carry = 0;
+        
+//         System.out.println("dummyHead data = " + dummyHead.val);
+//         System.out.println("dummyHead 주소값 = " + dummyHead);
+//         System.out.println("tail 주소값 = " + tail);
+        
+//         while (l1 != null || l2 != null || carry != 0) {
+//             int num1 = l1 == null ? 0 : l1.val;
+//             int num2 = l2 == null ? 0 : l2.val;
+//             int sum = num1 + num2 + carry;
+//             int val = sum % 10;
+//             carry = sum / 10;
+//             System.out.println("num1 = " + num1);
+//             System.out.println("num2 = " + num2);
+//             System.out.println("sum = " + sum);
+            
+//             ListNode newNode = new ListNode(val);
+//             tail.next = newNode;
+//             tail = newNode;
+            
+//             System.out.println("newNode = " + newNode);
+//             System.out.println("newNode.val = " + newNode.val);
+//             System.out.println("1 tail 주소값 = " + tail);
+//             System.out.println("tail.next 주소값 = " + tail.next);
+            
+//             l1 = (l1 != null) ? l1.next : null;
+//             l2 = (l2 != null) ? l2.next : null;
+//         }
+        
+//         return dummyHead.next;
     }
 }
