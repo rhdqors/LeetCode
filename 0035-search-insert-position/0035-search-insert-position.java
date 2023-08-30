2d7f38a1744309b126374b536154401c59c27b04
rhdqors <108318494+rhdqors@users.noreply.github.com>
2023-08-30 14:17:12 +0900

    Time: 0 ms (100.00%), Space: 43.6 MB (26.07%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -16,19 +16,5 @@ class Solution {
         }
 
         return left;
-        
-        
-        
-//         int start = 0;
-//         int end = nums.length-1;
-
-//         while (start <= end) {
-//             int mid = start + (end-start)/2;
-//             if (nums[mid] == target) return mid;
-//             else if (nums[mid] > target) end = mid-1;
-//             else start = mid+1;
-//         }
-
-//         return start;
     }
 }
