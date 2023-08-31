4ecedf1fc37779a289e486a31375efe3cff7f36e
rhdqors <108318494+rhdqors@users.noreply.github.com>
2023-08-31 14:23:15 +0900

    Time: 56 ms (5.88%), Space: 47.5 MB (6.53%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -2,40 +2,40 @@ class MinStack {
     private Node dummy;
     
     public MinStack() {
-        this.dummy = new Node(0, Integer.MAX_VALUE, null);
+        dummy = new Node(Integer.MAX_VALUE, 0, dummy);
     }
     
     public void push(int val) {
         int min = Math.min(dummy.min, val);
-        dummy = new Node(val, min, dummy);
-
+        dummy = new Node(min, val, dummy);
         System.out.println("min = " + min);
-        System.out.println("1 dummy = " + dummy);
+        System.out.println("data = " + dummy.data);
     }
     
     public void pop() {
-        dummy = dummy.head;
-        System.out.println("2 dummy = " + dummy);
-        // System.out.println("2 dummy.data = " + dummy.data);
+        System.out.println("pop = " + dummy.data);
+        dummy = dummy.next;
     }
-
+    
     public int top() {
+        System.out.println("top = " + dummy.data);
         return dummy.data;
     }
     
     public int getMin() {
+        System.out.println("getMin = " + dummy.min);
         return dummy.min;
     }
     
-    private static class Node {
-        int data;
+    static class Node {
         int min;
-        Node head;
+        int data;
+        Node next;
         
-        public Node(int data, int min, Node head) {
-            this.data = data;
+        Node(int min, int data, Node next) {
             this.min = min;
-            this.head = head;
+            this.data = data;
+            this.next = next;
         }
     }
 }
@@ -48,3 +48,56 @@ class MinStack {
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
  */
+
+
+
+
+// class MinStack {
+//     private Node dummy;
+    
+//     public MinStack() {
+//         this.dummy = new Node(0, Integer.MAX_VALUE, null);
+//     }
+    
+//     public void push(int val) {
+//         int min = Math.min(dummy.min, val);
+//         dummy = new Node(val, min, dummy);
+
+//         System.out.println("min = " + min);
+//         System.out.println("1 dummy = " + dummy);
+//     }
+    
+//     public void pop() {
+//         dummy = dummy.head;
+//         System.out.println("2 dummy = " + dummy);
+//     }
+
+//     public int top() {
+//         return dummy.data;
+//     }
+    
+//     public int getMin() {
+//         return dummy.min;
+//     }
+    
+//     private static class Node {
+//         int data;
+//         int min;
+//         Node head;
+        
+//         public Node(int data, int min, Node head) {
+//             this.data = data;
+//             this.min = min;
+//             this.head = head;
+//         }
+//     }
+// }
+
+// /**
+//  * Your MinStack object will be instantiated and called as such:
+//  * MinStack obj = new MinStack();
+//  * obj.push(val);
+//  * obj.pop();
+//  * int param_3 = obj.top();
+//  * int param_4 = obj.getMin();
+//  */
