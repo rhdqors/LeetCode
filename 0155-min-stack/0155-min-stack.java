88b8ee4cda8de5d1881c37455f0f04641886fd5a
rhdqors <108318494+rhdqors@users.noreply.github.com>
2023-08-31 14:56:32 +0900

    Time: 5 ms (77.28%), Space: 46.9 MB (87.08%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,41 +1,34 @@
 class MinStack {
-    private Node dummy;
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
     
     public MinStack() {
-        dummy = new Node(Integer.MAX_VALUE, 0, dummy);
+        stack = new Stack<>();
+        minStack = new Stack<>();
+        minStack.push(Integer.MAX_VALUE);
     }
     
     public void push(int val) {
-        int min = Math.min(dummy.min, val);
-        dummy = new Node(min, val, dummy);
-        System.out.println("min = " + min);
-        System.out.println("data = " + dummy.data);
+        stack.push(val);
+        int min = Math.min(getMin(), val);
+        minStack.push(min);
+        // System.out.println("stack = " + stack);
+        // System.out.println("min = " + min);
+        // System.out.println("minStack = " + minStack);
+        
     }
     
     public void pop() {
-        System.out.println("pop = " + dummy.data);
-        dummy = dummy.next;
+        stack.pop();
+        minStack.pop();
     }
     
     public int top() {
-        System.out.println("top = " + dummy.data);
-        return dummy.data;
+        return stack.peek();
     }
     
     public int getMin() {
-        System.out.println("getMin = " + dummy.min);
-        return dummy.min;
+        return minStack.peek();
     }
     
-    static class Node {
-        int min;
-        int data;
-        Node next;
-        
-        Node(int min, int data, Node next) {
-            this.min = min;
-            this.data = data;
-            this.next = next;
-        }
-    }
 }
