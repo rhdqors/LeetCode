fb15e03e558bf710bc04dd68cd8ca248e217c8c3
rhdqors <108318494+rhdqors@users.noreply.github.com>
2023-09-01 03:22:10 +0900

    Time: 47 ms (34.86%), Space: 43.2 MB (97.75%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,28 +1,28 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int[] arr = new int[2];
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         int[] arr = new int[2];
+        
+//         for(int i=0; i<nums.length; i++) {
+//             int minus = target - nums[i];
+//             if(map.containsKey(minus)) {
+//                 arr[0] = map.get(minus);
+//                 arr[1] = i;
+//                 return arr;
+//             }
+//             map.put(nums[i], i);
+//         }
+        
+//         return arr;
+        
         
         for(int i=0; i<nums.length; i++) {
-            int minus = target - nums[i];
-            if(map.containsKey(minus)) {
-                arr[0] = map.get(minus);
-                arr[1] = i;
-                return arr;
+            for(int j=i+1; j<nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                   return new int[] {i, j};
+                }
             }
-            map.put(nums[i], i);
         }
-        
-        return arr;
-        
-        
-        // for(int i=0; i<nums.length; i++) {
-        //     for(int j=i+1; j<nums.length; j++) {
-        //         if (nums[i] + nums[j] == target) {
-        //            return new int[] {i, j};
-        //         }
-        //     }
-        // }
-        // return new int[0];
+        return new int[0];
     }
 }
