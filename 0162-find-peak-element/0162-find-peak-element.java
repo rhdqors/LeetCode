c0b8161a0be5961df375ddfa9cd8ac91d4d5e8c0
rhdqors <108318494+rhdqors@users.noreply.github.com>
2023-09-06 21:49:49 +0900

    Time: 15 ms (100.00%), Space: 43.5 MB (5.51%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,27 +1,37 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        if(nums.length == 1) return 0;
-        
         int left = 0;
-        int right = nums.length - 1;
-
+        int right = nums.length-1;
+        System.out.println(left + ", " + right);
+        
+        if(nums.length == 1) {
+            return 0;
+        }
+        
         while(left <= right) {
+            // int mid = (left+right)/2;
             int mid = left + (right - left) / 2;
+            int target = nums[mid];
             
-            if(mid == 0 && nums[mid] > nums[mid+1]) {
+            if(mid == 0 && nums[mid] > nums[mid+1]) { // peak가 첫 요소
                 return mid;
             }
-            if(mid == nums.length-1 && nums[mid] > nums[mid-1]) {
+            if(mid == nums.length-1 && nums[mid] > nums[mid-1]) { // peak가 끝 요소
                 return mid;
             }
-            if(mid > 0 && mid < nums.length - 1 && nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) {
+            if( mid > 0 && mid < nums.length - 1 && target > nums[mid-1] && target > nums[mid+1]) {
+                System.out.println(" 1 = " + left + ", " + right + ", " + mid);
+                System.out.println("target = " + target);
                 return mid;
-            } else if(mid > 0 && nums[mid] < nums[mid-1]) {
+            } else if(mid > 0 && target < nums[mid-1]) { // mid가 0보다 클 때
+                System.out.println(" 2 = " + left + ", " + right + ", " + mid);
                 right = mid - 1;
-            } else {
+            } else { // mid가 0보다 작을 때
+                System.out.println(" 3 = " + left + ", " + right + ", " + mid);
                 left = mid + 1;
             }
         }
+        
         return left;
     }
 }
