da7bb524f21d842baa9c5a5ce7a7e0d1341680ef
rhdqors <108318494+rhdqors@users.noreply.github.com>
2023-09-07 21:28:07 +0900

    Time: 68 ms (5.11%), Space: 44 MB (7.24%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
@@ -14,20 +14,44 @@
  * }
  */
 class Solution {
-    Integer prev = null, min = Integer.MAX_VALUE;
-
+    TreeNode head;
+    Integer min = Integer.MAX_VALUE;
+    Integer data = null;
+    
     public int getMinimumDifference(TreeNode root) {
-        repeatForSort(root);
+        repeat(root);
         return min;
     }
-
-    private void repeatForSort(TreeNode node) {
-        if (node == null) return;
-        repeatForSort(node.left);
-        
-        if (prev != null) min = Math.min(min, node.val - prev);
-        prev = node.val;
-        
-        repeatForSort(node.right);
+    
+    public void repeat(TreeNode node) {
+        if(node == null) return ;
+        repeat(node.left);
+        if(data != null) {
+            min = Math.min(min, node.val-data);
+        }
+        data = node.val;
+        System.out.println("min = " + min);
+        System.out.println("data = " + data);
+        repeat(node.right);
     }
 }
+
+
+// class Solution {
+//     Integer prev = null, min = Integer.MAX_VALUE;
+
+//     public int getMinimumDifference(TreeNode root) {
+//         repeatForSort(root);
+//         return min;
+//     }
+
+//     private void repeatForSort(TreeNode node) {
+//         if (node == null) return;
+//         repeatForSort(node.left);
+        
+//         if (prev != null) min = Math.min(min, node.val - prev);
+//         prev = node.val;
+        
+//         repeatForSort(node.right);
+//     }
+// }
