c5cf47dfcbf02c9c8aa84534b301739161133927
rhdqors <108318494+rhdqors@users.noreply.github.com>
2023-09-07 18:01:32 +0900

    Time: 0 ms (100.00%), Space: 43.5 MB (25.21%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
@@ -15,49 +15,14 @@
  */
 class Solution {
     Integer prev = null, min = Integer.MAX_VALUE;
-    public int getMinimumDifference(TreeNode root) {
-        repeat(root);
-        return min;
-        
-        
-    }
     
-    public void repeat(TreeNode node) {
-        if(node == null) {
-            return; 
-        }
-        repeat(node.left);
-        System.out.println("node.val = " + node.val);
-        if(prev != null) {
-            min = Math.min(min, node.val-prev);
-        }
-        System.out.println("1 prev = " + prev);
-        prev = node.val;
-        System.out.println("2 prev = " + prev);
-        repeat(node.right);
+    public int getMinimumDifference(TreeNode root) {
+        if(root == null) return min;
+        
+        getMinimumDifference(root.left);
+        if(prev != null) min = Math.min(min, root.val-prev); 
+        prev = root.val;        
+        getMinimumDifference(root.right);
+        return min;
     }
 }
-
-// class Solution {
-//     public int getMinimumDifference(TreeNode root) {
-//         int min = Integer.MAX_VALUE;
-//         TreeNode pre = null;
-//         TreeNode cur = root;
-//         Stack<TreeNode> stack = new Stack<>();
-
-//         while (cur != null || !stack.isEmpty()) {
-//             while (cur != null) {
-//                 stack.push(cur);
-//                 cur = cur.left;
-//             }
-//             cur = stack.pop();
-//             if (pre != null) {
-//                 min = Math.min(min, cur.val - pre.val);
-//             }
-//             pre = cur;
-//             cur = cur.right;
-//         }
-
-//         return min;
-//     }
-// }
