4b9e054b0beaa93c84a6bd4d8158455d28a8b81c
rhdqors <108318494+rhdqors@users.noreply.github.com>
2023-09-07 19:29:58 +0900

    Time: 0 ms (100.00%), Space: 43.5 MB (25.21%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
@@ -31,19 +31,3 @@ class Solution {
         repeatForSort(node.right);
     }
 }
-
-
-// class Solution {
-//     Integer prev = null, min = Integer.MAX_VALUE;
-    
-//     public int getMinimumDifference(TreeNode root) {
-//         if(root == null) return min;
-//         getMinimumDifference(root.left);
-        
-//         if(prev != null) min = Math.min(min, root.val-prev); 
-//         prev = root.val;        
-        
-//         getMinimumDifference(root.right);
-//         return min;
-//     }
-// }
