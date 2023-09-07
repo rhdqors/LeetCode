12f030fdb1dcfc98452bd0833d63c7c3d21ea171
rhdqors <108318494+rhdqors@users.noreply.github.com>
2023-09-07 18:04:09 +0900

    Time: 1 ms (50.79%), Space: 43.2 MB (43.50%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
@@ -13,39 +13,39 @@
  *     }
  * }
  */
-class Solution {
-    Integer prev = null, min = Integer.MAX_VALUE;
-
-    public int getMinimumDifference(TreeNode root) {
-        repeat(root);
-        return min;
-    }
-
-    private void repeat(TreeNode node) {
-        if (node == null) {
-            return;
-        }
-        repeat(node.left);
-        if (prev != null) {
-            min = Math.min(min, node.val - prev);
-        }
-        prev = node.val;
-        repeat(node.right);
-    }
-}
-
-
 // class Solution {
 //     Integer prev = null, min = Integer.MAX_VALUE;
-    
+
 //     public int getMinimumDifference(TreeNode root) {
-//         if(root == null) return min;
-//         getMinimumDifference(root.left);
-        
-//         if(prev != null) min = Math.min(min, root.val-prev); 
-//         prev = root.val;        
-        
-//         getMinimumDifference(root.right);
+//         repeat(root);
 //         return min;
 //     }
+
+//     private void repeat(TreeNode node) {
+//         if (node == null) {
+//             return;
+//         }
+//         repeat(node.left);
+//         if (prev != null) {
+//             min = Math.min(min, node.val - prev);
+//         }
+//         prev = node.val;
+//         repeat(node.right);
+//     }
 // }
+
+
+class Solution {
+    Integer prev = null, min = Integer.MAX_VALUE;
+    
+    public int getMinimumDifference(TreeNode root) {
+        if(root == null) return min;
+        getMinimumDifference(root.left);
+        
+        if(prev != null) min = Math.min(min, root.val-prev); 
+        prev = root.val;        
+        
+        getMinimumDifference(root.right);
+        return min;
+    }
+}
