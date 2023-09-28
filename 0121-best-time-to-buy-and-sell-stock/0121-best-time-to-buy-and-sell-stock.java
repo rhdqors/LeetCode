c58935d6da68c8ec0be7605b20fdda094728bc92
rhdqors <108318494+rhdqors@users.noreply.github.com>
2023-09-28 16:14:02 +0900

    Time: 28 ms (9.93%), Space: 56.7 MB (97.15%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -2,23 +2,68 @@ class Solution {
     public int maxProfit(int[] prices) {
         int min = prices[0];
         int max = 0;
+
+        for(int i=1; i<prices.length; i++) {
+            if(min > prices[i]) {
+                min = prices[i];
+                System.out.println("min = " + min);
+            } else if(prices[i] - min > max) {
+                max = prices[i] - min;
+                System.out.println("max = " + max);
+            }
+        }
         
-        System.out.println("1 min = " + min);
-        System.out.println("1 max = " + max);
         
-        for (int i=1; i<prices.length; i++) {
-			if (min > prices[i]) {
-				min = prices[i];
-                System.out.println("2 min = " + min);
-			} else if (prices[i] - min > max) {
-				System.out.println("prices[i] = " + prices[i]);
-                System.out.println("3 min = " + min);
-				max = prices[i] - min;
-				System.out.println("2 max = " + prices[i] + " - " + min + " = " +  max);
-			}
-			System.out.println("4min = " + min);
-		}
-		System.out.println("3 max = " + max);
+        
+        
+        
         return max;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int min = prices[0];
+//         int max = 0;
+        
+//         System.out.println("1 min = " + min);
+//         System.out.println("1 max = " + max);
+        
+//         for (int i=1; i<prices.length; i++) {
+// 			if (min > prices[i]) {
+// 				min = prices[i];
+//                 System.out.println("2 min = " + min);
+// 			} else if (prices[i] - min > max) {
+// 				System.out.println("prices[i] = " + prices[i]);
+//                 System.out.println("3 min = " + min);
+// 				max = prices[i] - min;
+// 				System.out.println("2 max = " + prices[i] + " - " + min + " = " +  max);
+// 			}
+// 			System.out.println("4min = " + min);
+// 		}
+// 		System.out.println("3 max = " + max);
+//         return max;
+//     }
+// }
