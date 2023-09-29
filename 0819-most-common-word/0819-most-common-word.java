4a9f0508e732f064d00fb86a851fb900c3ccb9ca
rhdqors <108318494+rhdqors@users.noreply.github.com>
2023-09-29 18:23:18 +0900

    Time: 29 ms (7.22%), Space: 44.7 MB (6.60%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        String result = "";
+        String[] arr = paragraph.toLowerCase().split("\\W+");
+        
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = arr[i].replaceAll("[^a-zA-Z0-9]", "");
+        }
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        int max = 0;
+        
+        for(int i=0; i<arr.length; i++) {            
+            if(!map.containsKey(arr[i])) {
+                map.put(arr[i], 1);
+            } else {
+                int cnt = map.get(arr[i]);
+                map.put(arr[i], cnt + 1);    
+            }
+        }
+        
+        HashSet<String> set = new HashSet<>(Arrays.asList(banned));
+        int maxCnt = 0;
+        
+        for(Map.Entry<String, Integer> entry : map.entrySet()) {
+            if(!set.contains(entry.getKey()) && entry.getValue() > maxCnt) {
+                result = entry.getKey();
+                maxCnt = entry.getValue();
+            }
+        }
+        
+        return result;
+    }
+}
