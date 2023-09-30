2db09a77935ad3d1f83bc1c33cd3aa5de7a4c7f1
rhdqors <108318494+rhdqors@users.noreply.github.com>
2023-09-30 17:26:54 +0900

    Time: 13 ms (38.85%), Space: 44.9 MB (73.66%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -2,18 +2,10 @@ class Solution {
     public int arrayPairSum(int[] nums) {
         Arrays.sort(nums);
         int result = 0;
-        System.out.println("nums = " + Arrays.toString(nums));
-        
         
         for(int i=1; i<=nums.length; i++) {
-            // int min = Integer.MAX_VALUE;
-            System.out.println("nums[i-1] = " + nums[i-1]);
-            System.out.println("nums[i] = " + nums[i]);
-            
             int min = Math.min(nums[i-1], nums[i]);
-            System.out.println("min = " + min);
             result += min;
-            System.out.println("result = " + result);
 
             i++;
         }
