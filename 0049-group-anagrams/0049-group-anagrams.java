97f5ad6ec993b2955d667ef5ea50fe45d6323a29
rhdqors <108318494+rhdqors@users.noreply.github.com>
2023-09-30 14:22:56 +0900

    Time: 6 ms (73.31%), Space: 46.1 MB (98.70%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -3,20 +3,16 @@ class Solution {
         char[] chars = null;
         HashMap<String, ArrayList<String>> map = new HashMap<>();
         
-        
         for(int i=0; i<strs.length; i++) {
             chars = strs[i].toCharArray();
             Arrays.sort(chars);
-            System.out.println("chars 1 = " + Arrays.toString(chars)); // chars배열 요소별 정렬 완료
-            System.out.println("strs[i] = " + strs[i]);
             String str = new String(chars);
-            System.out.println("str = " + str);
+            
             if(!map.containsKey(str)) {
                 map.put(str, new ArrayList<String>());
             }
             map.get(str).add(strs[i]);
         }
-        System.out.println("map = " + map);
         
         return new ArrayList<>(map.values());
     }
