36d53b2c8963b7d3de618536873e2fa580904e2b
rhdqors <108318494+rhdqors@users.noreply.github.com>
2023-11-21 13:17:59 +0900

    Time: 0 ms (100.00%), Space: 55.9 MB (43.51%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,26 +1,22 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        k = k % nums.length;
-        int end = nums.length-1;
-        List<Integer> list = Arrays.stream(nums).boxed().collect(Collectors.toList());
-        List<Integer> kLists = new ArrayList<>();
+        int left = 0;
+        int right = nums.length - 1;
+        k %= nums.length;
         
-        System.out.println("list = " + list);
-        
-        for (int i=0; i<k; i++) {
-            kLists.add(list.get(end-i));
-            list.remove(end-i);
-        }
-        System.out.println("result1 = " + list);
-        System.out.println("result2 = " + kLists);
-        
-        for (int i=0; i< kLists.size(); i++) {
-            list.add(0,kLists.get(i));
-        }
-
-        System.out.println("result3 = " + list);
-        for (int i=0; i<nums.length; i++) {
-            nums[i] = list.get(i);
+        reverse(nums, left, right);
+        reverse(nums, left, k-1);
+        reverse(nums, k, right);
+    }
+    
+    public void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
         }
     }
+    
 }
