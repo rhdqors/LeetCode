851033e54019ddc1d8dbd8a738097f1dd9d32694
rhdqors <108318494+rhdqors@users.noreply.github.com>
2023-11-21 13:31:46 +0900

    Time: 0 ms (100.00%), Space: 43.2 MB (96.57%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -3,12 +3,10 @@ class Solution {
         int j = 2;
         
         for (int i=2; i<nums.length; i++) {
-            if(nums[i] != nums[j-2]) {
-                nums[j] = nums[i];
-                j++;
+            if (nums[i] != nums[j-2]) {
+                nums[j++] = nums[i];
             }
         }
-        
         return j;
     }
 }
